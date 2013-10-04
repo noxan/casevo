41d9f4ea787d349a8d68db98af3759e8024c18a0
noxan <noxan@byteweaver.net>
2013-10-04 11:50:02 +0200

    improve list display of transaction admin

--- casevo/transactions/admin.py
+++ casevo/transactions/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from casevo.transactions.models import Transaction
 
 
-admin.site.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'source', 'target', 'value')
+
+
+admin.site.register(Transaction, TransactionAdmin)
