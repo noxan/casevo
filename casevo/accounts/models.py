8ed729946d0e358da5ee93340f56b6999c7498f2
noxan <noxan@byteweaver.net>
2013-10-04 09:42:56 +0200

    add unicode method to account model

--- casevo/accounts/models.py
+++ casevo/accounts/models.py
@@ -6,3 +6,6 @@ from currencies.models import Currency
 class Account(models.Model):
     identifier = models.CharField(max_length=255)
     currency = models.ForeignKey(Currency, default=0)
+
+    def __unicode__(self):
+        return self.identifier
