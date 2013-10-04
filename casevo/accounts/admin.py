daceee22179c9536d30ca5f4035d52f1a48021c0
noxan <noxan@byteweaver.net>
2013-10-04 09:43:08 +0200

    register account and transaction model in admin

---
+++ casevo/accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from casevo.accounts.models import Account
+
+
+admin.site.register(Account)
---
+++ casevo/transactions/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from casevo.transactions.models import Transaction
+
+
+admin.site.register(Transaction)
