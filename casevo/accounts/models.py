bc93404560bb8fe0337b24b0f2321d3694be39fd
noxan <noxan@byteweaver.net>
2013-10-06 15:10:41 +0200

    add number field to account model

--- casevo/accounts/models.py
+++ casevo/accounts/models.py
@@ -5,6 +5,7 @@ from currencies.models import Currency
 
 class Account(models.Model):
     identifier = models.CharField(max_length=255, unique=True)
+    number = models.CharField(max_length=100)
     currency = models.ForeignKey(Currency, default=0)
 
     def __unicode__(self):
