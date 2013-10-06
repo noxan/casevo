38e1d134075575f6443c35293a525792fc091c5e
noxan <noxan@byteweaver.net>
2013-10-06 15:10:31 +0200

    add unique flag to account identifier

--- casevo/accounts/models.py
+++ casevo/accounts/models.py
@@ -4,7 +4,7 @@ from currencies.models import Currency
 
 
 class Account(models.Model):
-    identifier = models.CharField(max_length=255)
+    identifier = models.CharField(max_length=255, unique=True)
     currency = models.ForeignKey(Currency, default=0)
 
     def __unicode__(self):
