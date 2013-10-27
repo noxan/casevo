1f54cb821b362cab55b653333c91319fd094ffd5
noxan <noxan@byteweaver.net>
2013-10-27 14:11:54 +0100

    setup logger for casevo importers in settings

--- casevo/settings/__init__.py
+++ casevo/settings/__init__.py
@@ -160,12 +160,21 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console': {
+          'level': 'DEBUG',
+          'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
+            'propagate': True,
+        },
+        'casevo.importers': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     }
