f324fce3fe00168f7d0b8e5f6347b4b7c0a0b946
noxan <noxan@byteweaver.net>
2013-11-01 17:48:01 +0100

    add settings for django debug toolbar to development settings file

--- casevo/settings/development.py
+++ casevo/settings/development.py
@@ -7,3 +7,15 @@ DATABASES = {
         'NAME': 'casevo.db',
     }
 }
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'debug_toolbar',
+)
