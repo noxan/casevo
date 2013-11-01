1efc344eb52e56c7828dfd34f64c9c2408e8d01e
noxan <noxan@byteweaver.net>
2013-11-01 17:13:11 +0100

    create an account update view, template and url entry

--- casevo/accounts/urls.py
+++ casevo/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from casevo.accounts.views import AccountListView
+from casevo.accounts.views import AccountUpdateView, AccountListView
 
 
 urlpatterns = patterns('',
     url(r'^$', AccountListView.as_view(), name='list'),
+    url(r'^(?P<pk>[\d]+)/$', AccountUpdateView.as_view(), name='update'),
 )
--- casevo/accounts/views.py
+++ casevo/accounts/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import UpdateView, ListView
+from django.core.urlresolvers import reverse_lazy
 
 from casevo.accounts.models import Account
 
 
 class AccountListView(ListView):
     model = Account
+
+
+class AccountUpdateView(UpdateView):
+    model = Account
+    template_name = 'accounts/account_update.html'
+    success_url = reverse_lazy('accounts:list')
