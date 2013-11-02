01d85437bcc92c0cc672c978dc2ca774c9f0b133
noxan <noxan@byteweaver.net>
2013-11-02 19:43:44 +0100

    add a view, url and template for accounts pie chart

--- casevo/statistics/urls.py
+++ casevo/statistics/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from casevo.statistics.views import StatisticHomeView
+from casevo.statistics.views import StatisticHomeView, StatisticAccountsPieView
 
 
 urlpatterns = patterns('',
     url(r'^$', StatisticHomeView.as_view(), name='home'),
+    url(r'^accounts/pie/$', StatisticAccountsPieView.as_view(), name='accounts-pie'),
 )
 
--- casevo/statistics/views.py
+++ casevo/statistics/views.py
@@ -1,5 +1,24 @@
 from django.views.generic import TemplateView
 
+from casevo.accounts.models import Account
+
 
 class StatisticHomeView(TemplateView):
     template_name = 'statistics/statistic_home.html'
+
+
+class StatisticAccountsPieView(TemplateView):
+    template_name = 'statistics/statistic_accounts_pie.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(StatisticAccountsPieView, self).get_context_data(**kwargs)
+        context['accounts_in'] = []
+        context['accounts_out'] = []
+        context['accounts'] = Account.objects.all()
+        for account in context['accounts']:
+            if account.balance() > 0:
+                context['accounts_in'].append(account)
+            else:
+                context['accounts_out'].append(account)
+
+        return context
