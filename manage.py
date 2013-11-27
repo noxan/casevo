be7fa87402b547a0bfc0edc6b13779b8fe513c6c
noxan <noxan@byteweaver.net>
2013-11-27 22:40:52 +0100

    add target selection to manage.py

--- manage.py
+++ manage.py
@@ -2,8 +2,16 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casevo.settings")
+    target = 'development'
+    if os.path.isfile('.target'):
+        target = open('.target', 'r').readline().strip()
+        if not target or not os.path.isfile('casevo/settings/%s' % target):
+            sys.exit("Unknown target '%s'" % target)
+
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casevo.settings.%s" % target)
 
     from django.core.management import execute_from_command_line
 
