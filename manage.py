ee7da92ac24c3368b02227546f401964d65e6bc8
noxan <noxan@byteweaver.net>
2013-11-28 10:33:26 +0100

    add missing .py to settings parsing

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     target = 'development'
     if os.path.isfile('.target'):
         target = open('.target', 'r').readline().strip()
-        if not target or not os.path.isfile('casevo/settings/%s' % target):
+        if not target or not os.path.isfile('casevo/settings/%s.py' % target):
             sys.exit("Unknown target '%s'" % target)
 
 
